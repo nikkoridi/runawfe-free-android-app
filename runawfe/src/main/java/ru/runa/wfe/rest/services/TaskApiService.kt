45e7c654b91ccc6ae34090d315ec05ad2c546701
nikkoridi <git_kelpiecode@yahoo.com>
2025-11-06 14:08:39 +0300

    Try to convert tasks to native paged list

--- runawfe/src/main/java/ru/runa/wfe/rest/services/TaskApiService.kt
+++ runawfe/src/main/java/ru/runa/wfe/rest/services/TaskApiService.kt
@@ -1,6 +1,8 @@
 package ru.runa.wfe.rest.services
 
+import androidx.paging.PagingData
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Path
 import ru.runa.wfe.rest.dto.WfePagedList
@@ -9,7 +11,7 @@ import ru.runa.wfe.rest.dto.WfeTask
 interface TaskApiService {
 
     @POST("task/my/")
-    suspend fun getMyTasks(): Response<WfePagedList<WfeTask>>
+    suspend fun getMyTasks(@Body pagedList: PagingData<WfeTask>): Response<WfePagedList<WfeTask>>
 
     @POST("task/process/{processId}/")
     suspend fun getProcessTasks(@Path("processId") processId: Long): Response<List<WfeTask>>
