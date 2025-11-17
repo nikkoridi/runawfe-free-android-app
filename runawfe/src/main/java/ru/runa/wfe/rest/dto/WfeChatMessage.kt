93273ca8243ce92b91be85c83618aa0611f426c9
AliceKiseleva <aliseeva03@yandex.ru>
2025-11-17 14:40:18 +0300

    Fix dto WfeChatMessage: Date must be set

--- runawfe/src/main/java/ru/runa/wfe/rest/dto/WfeChatMessage.kt
+++ runawfe/src/main/java/ru/runa/wfe/rest/dto/WfeChatMessage.kt
@@ -7,5 +7,5 @@ data class WfeChatMessage(
     var text: String = "",
     var files: List<ChatMessageFileDetailDto> = emptyList(),
     var author: String = "",
-    var createDate: Date? = null // TODO: check is it ok
+    val createDate: Date
 )
