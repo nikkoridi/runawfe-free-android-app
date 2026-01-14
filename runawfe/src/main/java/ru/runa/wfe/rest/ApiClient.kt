25b0922a0b8c298fce8d63eec2ccd80eb89df505
AliceKiseleva <aliseeva03@yandex.ru>
2026-01-14 10:44:32 +0300

    Add check is url in settings a Runa server

--- runawfe/src/main/java/ru/runa/wfe/rest/ApiClient.kt
+++ runawfe/src/main/java/ru/runa/wfe/rest/ApiClient.kt
@@ -1,25 +1,71 @@
 package ru.runa.wfe.rest
 
+import android.net.Uri
+import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import ru.runa.wfe.rest.services.AuthApiService
 import ru.runa.wfe.rest.services.ChatApiService
 import ru.runa.wfe.rest.services.TaskApiService
+import java.net.SocketTimeoutException
 import java.util.concurrent.TimeUnit
 
 object ApiClient {
-    private const val BASE_URL: String = "http://10.0.2.2:8080/restapi/" // DEBUG DRAFT
+    private var BASE_URL: String = "http://10.0.2.2:8080/restapi/"
 
     val tokenManager: TokenManager = TokenManager
+
+    fun setServerUrl(url: String) {
+        val baseUrl = getBaseUrl(url)
+        if (checkServer(baseUrl)) {
+            BASE_URL = "$baseUrl/restapi/"
+        }
+        else {
+            Log.e("API Client", "Given URL is not a RunaWFE server")
+        }
+    }
 
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(ApiInterceptor())
         .callTimeout(1, TimeUnit.MINUTES)
         .build()
+
+    private fun getBaseUrl(url: String): String {
+        try {
+            val baseUrl = Uri.parse(url)
+            val port = if (baseUrl.port != -1) ":${baseUrl.port}" else ""
+            return "${baseUrl.scheme}://${baseUrl.host}$port"
+        } catch (e: Exception) {
+            Log.e("API Client", "Invalid URL")
+            return ""
+        }
+    }
+
+    private fun checkServer(url: String): Boolean {
+        val versionRequest = Request.Builder()
+            .url("$url/wfe/version/")
+            .build()
+        var version = ""
+        CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    version = OkHttpClient.Builder()
+                        .connectTimeout(2, TimeUnit.MINUTES)
+                        .build()
+                        .newCall(versionRequest).execute().body?.string() ?: ""
+                }  catch (e: SocketTimeoutException) {
+                    Log.e("API Client", e.message.toString())
+                }
+        }
+        return version.isNotBlank()
+    }
 
     private val gson: Gson = GsonBuilder()
         .setDateFormat("dd.MM.yyyy HH:mm")
--- runawfe/src/main/java/ru/runa/wfe/rest/ApiInterceptor.kt
+++ runawfe/src/main/java/ru/runa/wfe/rest/ApiInterceptor.kt
@@ -4,6 +4,7 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 class ApiInterceptor: Interceptor {
+    // TODO: SocketTimeout Exception
     override fun intercept(chain: Interceptor.Chain): Response {
         val modifiedRequest = chain.request().newBuilder()
             .addHeader("Content-Type", "application/json")
